770f367a30c2ecb5ca4fbda1024d4e1137b0bfee
Andrei <andreigrom33@gmail.com>
2024-05-13 12:21:00 +0300

    check through flake8,mypy

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,16 @@
 from typing import Optional
 
 
-
-def return_new_list(dictionary_list: list, state='CANCELED') -> Optional[list]:
+def return_new_list(dictionary_list: list, state="CANCELED") -> Optional[list]:
     """принимает список и сортирует его по заданному значению"""
     return_list = []
     for i in dictionary_list:
-        if i.get('state') == state:
+        if i.get("state") == state:
             return_list.append(i)
     return return_list
 
 
-def get_sorted_list(dictionary_list: list,ascending=True) -> Optional[list]:
+def get_sorted_list(dictionary_list: list, ascending=True) -> Optional[list]:
     """Принимает список и сортирует его по по убыванию даты"""
-    sort = sorted(dictionary_list, key=lambda i: i.get('date', 0), reverse=ascending)
+    sort = sorted(dictionary_list, key=lambda i: i.get("date", 0), reverse=ascending)
     return sort
