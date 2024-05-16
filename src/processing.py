ac9a3923fe3b938f341597af84ae8ef8a9169869
Andrei <andreigrom33@gmail.com>
2024-05-16 20:59:21 +0300

    add def get_sorted_list

--- src/processing.py
+++ src/processing.py
@@ -8,3 +8,9 @@ def return_new_list(dictionary_list: list, state="CANCELED") -> Optional[list]:
         if i.get("state") == state:
             return_list.append(i)
     return return_list
+
+
+def get_sorted_list(dictionary_list: list, ascending=True) -> Optional[list]:
+    """Принимает список и сортирует его по по убыванию даты"""
+    sort = sorted(dictionary_list, key=lambda i: i.get("date", 0), reverse=ascending)
+    return sort
