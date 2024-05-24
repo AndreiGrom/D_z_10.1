92784891cd4899cd1f15f96d1978be7e9b9e073d
Andrei <andreigrom33@gmail.com>
2024-05-24 16:13:12 +0300

    Написал функцию операцииcard_number_generator

--- src/generators.py
+++ src/generators.py
@@ -1,62 +1,20 @@
-transactions = [
-    {
-        "id": 939719570,
-        "state": "EXECUTED",
-        "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "9824.07",
-            "currency": {"name": "USD", "code": "USD"},
-        },
-        "description": "Перевод организации",
-        "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702",
-    },
-    {
-        "id": 142264268,
-        "state": "EXECUTED",
-        "date": "2019-04-04T23:20:05.206878",
-        "operationAmount": {
-            "amount": "79114.93",
-            "currency": {"name": "USD", "code": "USD"},
-        },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 19708645243227258542",
-        "to": "Счет 75651667383060284188",
-    },
-    {
-        "id": 873106923,
-        "state": "EXECUTED",
-        "date": "2019-03-23T01:09:46.296404",
-        "operationAmount": {
-            "amount": "43318.34",
-            "currency": {"name": "руб.", "code": "RUB"},
-        },
-        "description": "Перевод со счета на счет",
-        "from": "Счет 44812258784861134719",
-        "to": "Счет 74489636417521191160",
-    },
-    {
-        "id": 895315941,
-        "state": "EXECUTED",
-        "date": "2018-08-19T04:27:37.904916",
-        "operationAmount": {
-            "amount": "56883.54",
-            "currency": {"name": "USD", "code": "USD"},
-        },
-        "description": "Перевод с карты на карту",
-        "from": "Visa Classic 6831982476737658",
-        "to": "Visa Platinum 8990922113665229",
-    },
-    {
-        "id": 594226727,
-        "state": "CANCELED",
-        "date": "2018-09-12T21:27:25.241689",
-        "operationAmount": {
-            "amount": "67314.70",
-            "currency": {"name": "руб.", "code": "RUB"},
-        },
-        "description": "Перевод организации",
-        "from": "Visa Platinum 1246377376343588",
-        "to": "Счет 14211924144426031657",
-    },
-]
+def filter_by_currency(transactions, valut="USD"):
+    usd = []
+    for k in transactions:
+        for i, v in k.items():
+            if i["currency"] == valut:
+                usd.append(i)
+
+
+def transaction_descriptions(list_inp):
+    result = (v for i in list_inp for k, v in i.items() if k == "description")
+    return result
+
+
+def card_number_generator(start=1, stop=9999999999999999):
+    while True:
+        result = f"{start:016d}"
+        yield result[:4] + " " + result[4:8] + " " + result[8:12] + " " + result[12:]
+        start += 1
+        if start == stop + 1:
+            break
