79a6b4f03193669670d53c2cd4c59222d79b4d92
Andrei <andreigrom33@gmail.com>
2024-12-05 23:42:55 +0300

    Написал функцию конвертации валют

--- src/external_api.py
+++ src/external_api.py
@@ -1,20 +1,48 @@
-date = [
-    {
-        "id": 441945886,
-        "state": "EXECUTED",
-        "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {
-            "amount": "31957.58",
-            "currency": {"name": "руб.", "code": "RUB"},
-        },
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+def convert_currency(amount, from_currency, to_currency, api_key):
+    # Убедитесь, что валюта поддерживается
+    supported_currencies = ['USD', 'EUR']
+    if from_currency not in supported_currencies or to_currency not in supported_currencies:
+        raise ValueError("Поддерживаемые  валюты USD и EUR.")
+
+    # Формируем URL для запроса
+    url = f"https://api.apilayer.com/exchangerates_data/latest?base={from_currency}&symbols={to_currency}"
+
+    headers = {
+        'apikey': api_key,
     }
-]
 
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Проверка на наличие ошибок
+        data = response.json()
 
-def tranzaction_summ(list):
-    for i in list:
-        if i["operationAmount"]["currency"]["code"] == "RUB":
-            return float(i["operationAmount"]["amount"])
+        # Получаем курс
+        exchange_rate = data['rates'].get(to_currency)
+        if exchange_rate is None:
+            raise ValueError(f"Не удалось найти обменный курс для {to_currency}")
 
+        # Конвертируем
+        converted_amount = amount * exchange_rate
+        return converted_amount
 
-print(tranzaction_summ(date))
+    except requests.exceptions.RequestException as e:
+        print(f"Не удалось выполнить HTTP-запрос : {e}")
+        return None
+    except ValueError as e:
+        print(e)
+        return None
+
+# Пример использования функции
+api_key = os.getenv('api_key')
+amount_to_convert = 100  # сумма в USD
+from_curr = 'USD'
+to_curr = 'EUR'
+
+converted_value = convert_currency(amount_to_convert, from_curr, to_curr, api_key)
+if converted_value is not None:
+    print(f"{amount_to_convert} {from_curr} is {converted_value:.2f} {to_curr}")
