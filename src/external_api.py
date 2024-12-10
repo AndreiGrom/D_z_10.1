2c79585302a0533368267d7b7cc5f8b42e95e95e
Andrei <andreigrom33@gmail.com>
2024-12-10 23:23:39 +0300

    Оформил код в сооответствии с PEP 8

--- src/external_api.py
+++ src/external_api.py
@@ -1,21 +1,26 @@
-import requests
 import os
+
+import requests
 from dotenv import load_dotenv
 
-load_dotenv('.env')
-api_key = os.getenv('api_key')
+load_dotenv(".env")
+api_key = os.getenv("api_key")
+
 
 def convert_currency(amount, from_currency, to_currency, api_key):
     # Убедитесь, что валюта поддерживается
-    supported_currencies = ['USD', 'EUR']
-    if from_currency not in supported_currencies or to_currency not in supported_currencies:
+    supported_currencies = ["USD", "EUR"]
+    if (
+        from_currency not in supported_currencies
+        or to_currency not in supported_currencies
+    ):
         raise ValueError("Поддерживаемые  валюты USD и EUR.")
 
     # Формируем URL для запроса
     url = f"https://api.apilayer.com/exchangerates_data/latest?base={from_currency}&symbols={to_currency}"
 
     headers = {
-        'apikey': api_key,
+        "apikey": api_key,
     }
 
     try:
@@ -24,7 +29,7 @@ def convert_currency(amount, from_currency, to_currency, api_key):
         data = response.json()
 
         # Получаем курс
-        exchange_rate = data['rates'].get(to_currency)
+        exchange_rate = data["rates"].get(to_currency)
         if exchange_rate is None:
             raise ValueError(f"Не удалось найти обменный курс для {to_currency}")
 
@@ -39,11 +44,12 @@ def convert_currency(amount, from_currency, to_currency, api_key):
         print(e)
         return None
 
+
 # Пример использования функции
-api_key = os.getenv('api_key')
+api_key = os.getenv("api_key")
 amount_to_convert = 100  # сумма в USD
-from_curr = 'USD'
-to_curr = 'EUR'
+from_curr = "USD"
+to_curr = "EUR"
 
 converted_value = convert_currency(amount_to_convert, from_curr, to_curr, api_key)
 if converted_value is not None:
